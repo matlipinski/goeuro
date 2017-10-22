78ae40e17e7586c4ab49839ab2d01fcb5091d122
Mateusz Lipiński <lipinski.mat@gmail.com>
2017-10-22 11:13:59 +0200

    New algorithm to verify route

--- src/main/java/pl/goeuro/bus/DirectRouteVerifier.java
+++ src/main/java/pl/goeuro/bus/DirectRouteVerifier.java
@@ -1,67 +1,39 @@
 package pl.goeuro.bus;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Stream;
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.TreeSet;
 
 @Service
 @Slf4j
 class DirectRouteVerifier {
 
-    @Value("${routes.file.path}")
-    private String routesFilePath;
+    private final RouteRepository routeRepository;
 
-    @PostConstruct
-    private void postConstruct() {
-        log.info("File path: {}", routesFilePath);
+    @Autowired
+    public DirectRouteVerifier(RouteRepository routeRepository) {
+        this.routeRepository = routeRepository;
     }
+
 
     Route verify(int departureStationId, int arrivalStationId) {
-        String departure = String.valueOf(departureStationId);
-        String arrival = String.valueOf(arrivalStationId);
-        log.debug("Try to find router for between stations: {} - {}", departure, arrival);
-        Path filePath = Paths.get(routesFilePath);
-        boolean foundDirectRoute = false;
-        try {
-            foundDirectRoute = Files.lines(filePath, StandardCharsets.UTF_8)
-                    .skip(1)
-                    .map(route -> route.split(" "))
-                    .anyMatch(route -> findRoute(route, departure, arrival));
 
-        } catch (IOException ex) {
-            log.error("Error while reading file with routes: {}", ex);
+        log.debug("Try to find router for between stations: {} - {}", departureStationId, arrivalStationId);
+
+        if (Objects.equals(departureStationId, arrivalStationId)) {
+            return new Route(departureStationId, arrivalStationId, true);
         }
 
-        return Route.builder()
-                .arrivalStationId(departureStationId)
-                .departureStationId(arrivalStationId)
-                .directBusRoute(foundDirectRoute)
-                .build();
-    }
+        TreeSet departureRoutes = routeRepository.getRoutes().getOrDefault(departureStationId, new TreeSet<>());
+        TreeSet arrivalRoutes = routeRepository.getRoutes().getOrDefault(arrivalStationId, new TreeSet<>());
 
-    private boolean findRoute(String[] busStations, String departure, String arrival) {
-        boolean alreadyFoundDeparture = false;
-        String stationToFind = departure;
-        for (int i = 1; i < busStations.length; i++) {
-            if (busStations[i].equals(stationToFind)) {
-                if (alreadyFoundDeparture) {
-                    log.info("Found route for departure:{}, arrival: {}", departure, arrival);
-                    return true;
-                }
-                stationToFind = arrival;
-                alreadyFoundDeparture = true;
-            }
-        }
-        return false;
+        boolean hasCommonStationId = !Collections.disjoint(departureRoutes, arrivalRoutes);
+        log.debug("Between stations {} - {}, has direct route: {}",
+                departureStationId, arrivalStationId, hasCommonStationId);
+        return new Route(departureStationId, arrivalStationId, hasCommonStationId);
     }
 }
